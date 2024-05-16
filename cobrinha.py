dac7761b0fa95f56d03370704dc8952e390c2273
Leonardo Barbosa <87045040+Leleco1604@users.noreply.github.com>
2024-05-16 11:40:29 -0300

    commit

---
+++ cobrinha.py
@@ -0,0 +1,53 @@
+class Cobra:
+    def __init__(self, tam_tela = (300, 400), 
+                        posicao = [80, 50], # esquerda, cima
+                        corpo = [[80, 50],[70, 50],[60,50]],
+                        direcao = 'DIREITA'):
+        self.tam_tela = tam_tela
+        self.posicao = posicao
+        self.corpo = corpo
+        self.direcao = direcao
+
+
+    def muda_direcao(self, nova_direcao):
+        if nova_direcao == 'DIREITA' and not self.direcao == 'ESQUERDA':
+            self.direcao = 'DIREITA'
+        if nova_direcao == 'ESQUERDA' and not self.direcao == 'DIREITA':
+            self.direcao = 'ESQUERDA'
+        if nova_direcao == 'CIMA' and not self.direcao == 'BAIXO':
+            self.direcao = 'CIMA'
+        if nova_direcao == 'BAIXO' and not self.direcao == 'CIMA':
+            self.direcao = 'BAIXO'
+
+
+    def move(self, posicao_comida):
+        if self.direcao == 'DIREITA':
+            self.posicao[0] += 10
+        if self.direcao == 'ESQUERDA':
+            self.posicao[0] -= 10
+        if self.direcao == 'BAIXO':
+            self.posicao[1] += 10
+        if self.direcao == 'CIMA':
+            self.posicao[1] -= 10
+
+        # Adiciona pedaço do corpo da cobra na cabeça
+        self.corpo.insert(0, list(self.posicao))
+
+        # Confere se comeu uma comida
+        if self.posicao == posicao_comida:
+            return True
+
+        # Se não comeu remove o último elemento
+        self.corpo.pop()
+        return False
+
+    def colisao(self):
+        if self.posicao[0] > (self.tam_tela[0] - 10) or self.posicao[0] < 0:
+            return True
+
+        if self.posicao[1] > (self.tam_tela[1] - 10) or self.posicao[1] < 0:
+            return True
+
+        for pedaco in self.corpo[1:]:
+            if self.posicao == pedaco:
+                return True
---
+++ jogo.py
@@ -0,0 +1,94 @@
+import pygame
+import sys
+import time
+
+from cobrinha import Cobra
+from comida import Comida
+
+# Inicia o pygame
+pygame.init()
+TAM_TELA = (300,400)
+tela = pygame.display.set_mode(TAM_TELA)
+
+# Para colocar uma fonte
+pygame.font.init()
+minha_font = pygame.font.SysFont('Ubuntu', 20)
+
+# Cronômetro
+tempo = pygame.time.Clock()
+
+# Som de comer
+pygame.mixer.pre_init(44100, -16, 2, 2048)
+def playNotificationSound():
+    pygame.mixer.music.load("som.ogg")
+    pygame.mixer.music.play()
+
+
+# som = pygame.mixer.Sound('som.mp3')
+
+cobra = Cobra()
+comida = Comida()
+posicao_comida = comida.posicao
+
+pontuacao = 0
+
+# Para ficar atualizando a tela
+while True:
+
+    # Muda a cor da tela, usando o rgb
+    tela.fill((25, 0, 34))
+
+    for event in pygame.event.get():
+        # escuta - mouse ou teclado, for igual a sair
+        if event.type == pygame.QUIT:
+            # Interrompe o jogo
+            pygame.quit()
+            print('falou')
+            # Fecha a janela
+            sys.exit()  
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                cobra.muda_direcao('DIREITA')   
+            if event.key == pygame.K_UP:
+                cobra.muda_direcao('CIMA')
+            if event.key == pygame.K_DOWN:
+                cobra.muda_direcao('BAIXO')
+            if event.key == pygame.K_LEFT:
+                cobra.muda_direcao('ESQUERDA')
+    
+    posicao_comida = comida.gera_nova_posicao()
+
+    # Se a posicao da cobra for igual a da comida
+    if cobra.move(posicao_comida):
+        comida.devorada = True
+        playNotificationSound()
+        pontuacao += 1 
+    
+    # Verifica colisão
+    if cobra.colisao():
+        pontos = minha_font.render(f'Você perdeu! pontos: {pontuacao}', True, (255,255,255))
+        tela.blit(pontos,(50,180))
+        pygame.display.flip()
+        time.sleep(1)
+        pygame.quit()
+        sys.exit()
+
+    # Texto da pontuação
+    pontos = minha_font.render(f'Pontuação: {pontuacao}', True, (255,255,255))
+    tela.blit(pontos,(10,10))
+    # Desenha a cobra na tela
+    for pos in cobra.corpo:
+        pygame.draw.rect(tela, pygame.Color(67,145,0),
+                                pygame.Rect(pos[0], pos[1], 10, 10))
+
+    # Desenha a comida na tela
+    pygame.draw.rect(tela, pygame.Color(150, 200, 100),
+                            pygame.Rect(posicao_comida[0], posicao_comida[1], 10, 10))
+
+    # Atualiza a tela a cada frame
+    pygame.display.update() 
+
+    # Define os frames do jogo
+    tempo.tick(16)
+    
